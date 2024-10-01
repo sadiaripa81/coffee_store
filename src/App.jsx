8bbb41801d79fd0dd7b91ed59e249a92b9e88151
sadia <sadiaripa81@gmail.com>
2024-10-01 20:03:09 +0600

    Commit message

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-6xl text-amber-950">Coffee store</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const addCoffee = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default addCoffee;
